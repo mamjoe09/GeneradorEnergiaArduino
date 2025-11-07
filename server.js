6c43dabf8d3ae3c41017dbb1710ecb4e99324dd1
Mateo Moglia <m.moglia2009@gmail.com>
2025-11-07 12:45:16 -0300

    Refactor server.js to use ES modules and add POST endpoint
    Refactor server setup to use ES modules and add new endpoint for Arduino data.

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-const express = require("express");
-const { SerialPort, ReadlineParser } = require("serialport");
-const http = require("http");
-const { Server } = require("socket.io");
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static("public"));
-
-// ⚙️ Cambiá "COM3" por el puerto real de tu Arduino
-const port = new SerialPort({ path: "COM14", baudRate: 9600 });
-const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
-
-parser.on("data", data => {
-  const [volt, curr, pow] = data.split(",");
-  io.emit("datos", { volt, curr, pow });
+const server = createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" },
 });
 
-server.listen(3000, () => console.log("🌐 Servidor en http://localhost:3000"));
+app.use(express.json());
+
+let ultimoDato = { volt: 0, curr: 0, pow: 0 };
+
+// 📥 Endpoint que recibe los datos del Arduino (desde tu PC)
+app.post("/datos", (req, res) => {
+  ultimoDato = req.body;
+  io.emit("datos", ultimoDato);
+  console.log("📈 Datos actualizados:", ultimoDato);
+  res.sendStatus(200);
+});
+
+// 🌐 Verificación
+app.get("/", (req, res) => res.send("Servidor online 🔥"));
+
+server.listen(3000, () => console.log("✅ Servidor Render activo"));
